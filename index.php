2e80ab3f3226817cdbddd0c7e246689d2fbd5b5b
Valery Dauzhuk <vandalv@tut.by>
2021-10-28 00:22:29 +0200

    music player implement

--- index.php
+++ index.php
@@ -15,9 +15,10 @@ else{
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <title>Mucic Stream</title>
 </head>
 <body>
-    <b>Hello</b>
+    <div id="playerBar"></div>
 </body>
 </html>
