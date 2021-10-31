ad4954fabbafd0893f65607e56ee928197cfe42f
Valery Dauzhuk <vandalv@tut.by>
2021-10-31 14:04:42 +0100

    Output to includes

---
+++ includes/navBarContainer.php
@@ -0,0 +1,23 @@
+<div id="navBarContainer">
+    <nav class="navBar">
+        <a href="index.php" class="logo">
+            <img src="assets/icons/logo.png">
+        </a>
+        <div class="group">
+            <div class="navItem">
+                <a href="search.php" class="navItemLink">Search<img src="assets/icons/search.png" class="icon" alt="search"></a>
+            </div>
+        </div>
+        <div class="group">
+            <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+            <div class="navItem">
+                <a href="musiclibrary.php" class="navItemLink">Music Library</a>
+            </div>
+            <div class="navItem">
+                <a href="profile.php" class="navItemLink">User Profile</a>
+            </div>
+        </div>
+    </nav>
+</div>
