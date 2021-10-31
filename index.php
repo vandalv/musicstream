4e42737c6d0a862955eee65c046fb5aab141d6a9
Valery Dauzhuk <vandalv@tut.by>
2021-10-31 12:17:08 +0100

    adding music and artwork

--- index.php
+++ index.php
@@ -28,7 +28,18 @@ else{
                     </a>
                     <div class="group">
                         <div class="navItem">
-                            <a href="search.php" class="navItemLink">Search</a>
+                            <a href="search.php" class="navItemLink">Search<img src="assets/icons/search.png" class="icon" alt="search"></a>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="musiclibrary.php" class="navItemLink">Music Library</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">User Profile</a>
                         </div>
                     </div>
                 </nav>
