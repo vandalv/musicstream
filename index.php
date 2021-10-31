a116ba224826f4e15c449b9619d38249a41511f1
Valery Dauzhuk <vandalv@tut.by>
2021-10-31 19:36:59 +0100

    main page decoration

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php include("includes/header.php")?>
-<h2 class="pageRec">You might also like:</h2>
+<h2 class="pageRec">Here are our recommendations for you:</h2>
 <div class="gridContainer">
     <?php 
         $albumQuery = mysqli_query($dbconnect, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
         while($row = mysqli_fetch_array($albumQuery)){
-            echo $row['title'] . "<br>";
+            echo "<div class='itemViewer'>
+            <a href='album.php?id=" . $row['id'] . "'>
+                <img src='{$row['artworkPath']}'>              
+                <div class='infoViewer'>                  
+                    {$row['title']}              
+                </div> 
+            </a>
+            </div>";
         }
     ?>
 </div>
