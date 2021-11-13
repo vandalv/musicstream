f48e99cb6ab146e56e91684f0d058d2b9e9c3dbd
Valery Dauzhuk <vandalv@tut.by>
2021-11-13 11:15:38 +0100

    ->

--- search.php
+++ search.php
@@ -9,6 +9,20 @@
 ?>
 
 <div class="searchContainer">
-    <input type="text" class="searchInput" placeholder="sin" name="sin" id='sin' required value="<?php echo $searchTerm;?>">
+    <input type="text" class="searchInput" onchange='val' id="searchI" required value="<?php echo $searchTerm;?>">
     <label for="sin" class="form__label">Search For Artist, Album Or Playlist:</label>
 </div>
+
+<script>
+    var timer;
+    var val;
+    $(".searchInput").keyup(function(){
+        clearTimeout(timer);
+
+        timer = setTimeout(function(){
+            val = $(".searchInput").val();
+            openPage("search.php?searchTetm=" + val);
+        }, 2000);
+    })
+    $(".searchInput").val(val);
+</script>
