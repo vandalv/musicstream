142aae5e0d83f96be09311348879c1b5e5bfff5b
Valery Dauzhuk <vandalv@tut.by>
2021-11-13 09:48:14 +0100

    ->

--- search.php
+++ search.php
@@ -1,7 +1,14 @@
 <?php 
     include("includes/includedFiles.php");
     if(isset($_GET['searchTerm'])){
-        $searchTerm = $_GET['searchTerm'];
-        echo $searchTerm;
+        $searchTerm = urldecode($_GET['searchTerm']);
+    }
+    else{
+        $searchTerm = "";
     }
 ?>
+
+<div class="searchContainer">
+    <input type="text" class="searchInput" placeholder="sin" name="sin" id='sin' required value="<?php echo $searchTerm;?>">
+    <label for="sin" class="form__label">Search For Artist, Album Or Playlist:</label>
+</div>
