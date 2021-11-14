f9632c48d17d9cdb6d2bd0c7abaffa96244036f6
Valery Dauzhuk <vandalv@tut.by>
2021-11-14 16:34:50 +0100

    ->

---
+++ musicLibrary.php
@@ -0,0 +1,12 @@
+<?php 
+include("includes/includedFiles.php");
+?>
+
+<div class="playlistContainer">
+    <div class="gridContainer">
+        <h2 class="pl">Playlists</h2>
+        <div class="buttonItems">
+            <button class="button">New Playlist</button>
+        </div>
+    </div>
+</div>
