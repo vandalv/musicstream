4f0ff36e1638c4af7f1d5a4767c7076d1d349f8a
Valery Dauzhuk <vandalv@tut.by>
2021-11-24 14:07:33 +0100

    ->

--- includes/ajax/updateEmail.php
+++ includes/ajax/updateEmail.php
@@ -5,7 +5,19 @@
         exit();
     }
     if(isset($_POST['email']) && $_POST['email'] != ""){
-        echo "Email Set";
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo "Email Is Invalid";
+            exit();
+        }
+        $emailCheck = mysqli_query($dbconnect, "SELECT email FROM accounts WHERE email='$email' AND username != '$username'");
+        if(mysqli_num_rows($emailCheck) > 0){
+            echo "Email Is Already In Use";
+            exit();
+        }
+        $q = mysqli_query($dbconnect, "UPDATE accounts SET email = '$email' WHERE username = '$username'");
+        echo "Update Was Sucessfull";
     }
     else{
         echo "You Must Provide An Email";
