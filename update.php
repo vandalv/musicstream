e2433819a0d18cd28355810629a227283c527497
Valery Dauzhuk <vandalv@tut.by>
2021-11-24 11:25:22 +0100

    ->

--- update.php
+++ update.php
@@ -1,1 +1,20 @@
-Hello!
+<?php 
+    include("includes/includedFiles.php");
+?>
+
+<?php 
+    $q = mysqli_query($dbconnect, "SELECT concat(firstName, ' ' ,lastName) AS 'full' FROM accounts WHERE username='$userL'");
+    $row = mysqli_fetch_array($q);
+    $result = $row['full'];
+    ?>
+    <h2><?php echo $result; ?></h2>
+
+<div class="userInfo">
+    <div class="container border">
+        <h2>EMAIL</h2>
+        <input type="text" class="email" placeholder="Update e-mail" value="echo $userLoggedIn->getEmail();">
+    </div>
+    <div class="container">
+        <h2>PASSWORD</h2>
+    </div>
+</div>
