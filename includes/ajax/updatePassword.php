6193e218f5c3f92870050a92be79f7cd3fc14f77
Valery Dauzhuk <vandalv@tut.by>
2021-11-24 15:11:05 +0100

    ->

--- includes/ajax/updatePassword.php
+++ includes/ajax/updatePassword.php
@@ -4,5 +4,32 @@
         echo "ERROR: NO USER SET";
         exit();
     }
-    
+    if(!isset($_POST['oldPassword']) || !isset($_POST['newPassword1']) || !isset($_POST['newPassword2'])){
+        echo "No Passwords Set";
+        exit();
+    }
+    if($_POST['oldPassword'] == "" || $_POST['newPassword1'] == "" || $_POST['newPassword2'] == ""){
+        echo "Not All Fillds Were Filled";
+        exit();
+    }
+    $username = $_POST['username'];
+    $oldPassword = $_POST['oldPassword'];
+    $newPassword1 = $_POST['newPassword1'];
+    $newPassword2 = $_POST['newPassword2'];
+
+    $oldMD5 = md5($oldPassword);
+    $pCheck = mysqli_query($dbconnect, "SELECT * FROM accounts WHERE username='username' AND password='$oldMD5'");
+    if(mysqli_num_rows($pCheck) != 1){
+        echo "Old Password Incorrect";
+        exit();
+    }
+    if($newPassword1 != $newPassword2){
+        echo "Passwords Do Not Match";
+        exit();
+    }
+    preg_match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", $newPassword1, $matchesP);
+        if ($matchesP){
+            $q = mysqli_query($dbconnect, "UPDATE accounts SET password = '$newPassword1' WHERE username = '$username'");
+            echo "Update Was Sucessfull";
+        }
 ?>
