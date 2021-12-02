80515f5b9f5bda4a89c384dc893b719c2b626690
Valery Dauzhuk <vandalv@tut.by>
2021-12-02 14:00:07 +0100

    ->

--- includes/config.php
+++ includes/config.php
@@ -8,6 +8,8 @@ $timezone = date_default_timezone_set("Europe/Warsaw");
 
 $dbconnect = mysqli_connect("localhost", "root", "", "musicstreamdb");
 
+// $dbconnect = mysqli_connect("sql212.epizy.com", "epiz_30500631", "e0fJJSpIgOIFdI9", "epiz_30500631_musicstream");
+
 if(mysqli_connect_errno()){
     echo "Unable to connect to database: " . mysqli_connect_errno();
 }
